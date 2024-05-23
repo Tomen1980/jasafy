30757edbcda1d7b771a634bced6553e0bbe41fb3
Tomen1980 <agungjumantoroandrian@gmail.com>
2024-05-24 00:25:23 +0700

    +users migrations fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,8 +20,8 @@ return new class extends Migration
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_number');
-            $table->enum('role',['seller','customer ']);
-            $table->string("image")->nullable();
+            $table->enum('role', ['seller', 'customer']);
+            $table->string('image')->nullable()->default("default.jpg");
             $table->rememberToken();
             $table->timestamps();
         });
